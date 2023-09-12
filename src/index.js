9ad5b19b88e87ed5ee894360e301cb0dca85af9c
swetha <swethaashok02@gmail.com>
2023-09-12 20:29:39 +0530

    Landing page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import AuthLayout from 'layouts/Auth.js';
 import AdminLayout from 'layouts/Admin.js';
+import AuthLayout from 'layouts/Auth.js';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 
@@ -13,7 +12,7 @@ ReactDOM.render(
       <Switch>
         <Route path={`/auth`} component={AuthLayout} />
         <Route path={`/admin`} component={AdminLayout} />
-        <Redirect from={`/`} to='/admin/dashboard' />
+        <Redirect from={`/`} to='/auth/signin' />
       </Switch>
     </HashRouter>
   </Provider>,
