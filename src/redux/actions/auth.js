4c2c92242bb0f94671ed8f71627144cd005b26d6
Vignesh-2811 <shakthivignesh2002@gmail.com>
2023-09-13 21:25:43 +0530

    feat: completed signin with jwt to local storage

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from '../../axios-api';
+import { ADD_JWT_TOKEN } from './types';
 
 export const submitRegisterEvent = (data) => async (dispatch) => {
   try {
@@ -8,8 +9,26 @@ export const submitRegisterEvent = (data) => async (dispatch) => {
       },
     };
     const res = await axios.post('/api/users/signup', data, config);
+    return res.status;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export const loginEvent = (data) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const res = await axios.post('/api/users/login', data, config);
+    dispatch({
+      type: ADD_JWT_TOKEN,
+      payload: res.data.accessToken,
+    });
     return res;
   } catch (error) {
-    console.log(error);
+    return error.response;
   }
 };
