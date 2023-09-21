91b6025c7dd858240f12670365896cc9ef3fb259
sri-lekha-19 <2021ad0414@svce.ac.in>
2023-09-21 20:09:01 +0530

    future assignment page

---
+++ src/views/Dashboard/Ass.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Assign = () => {
+  return (
+    <div>
+      <h2>Future Assignment</h2>
+      {/* Your Assignment details goes here */}
+    </div>
+  );
+};
+
+export default Assign;
